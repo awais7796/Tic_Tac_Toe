7c64d99d264ec34d327074fb5c6864858e65f9e0
awais7796 <awaitzz49@gmail.com>
2023-12-15 11:14:49 +0530

    changes made

--- app.js
+++ app.js
@@ -1,6 +1,8 @@
 let boxes = document.querySelectorAll(".box");
 let reset =document.querySelector("#reset");
-
+let newGameBtn= document.querySelector("#new-btn");
+let msgContainer =document.querySelector(".msg-container");
+let msg =document.querySelector("#msg");
 let turnO = true ;
 const winPattern =[
     [0,1,2],
@@ -12,7 +14,39 @@ const winPattern =[
     [0,4,8],
     [2,4,6]
 ];
-boxes.onclick=() => {
-    console.log("clicked");
+boxes.forEach( (box) => {
+    box.addEventListener("click",()=>{
+        console.log("box was clicked");
+        if(turnO){
+            box.innerText ="0";
+            turnO=false;
+        }
+        else{
+            box.innerText ="X";
+            turnO=true;
+        }
+        box.disabled= true;
+
+        checkWinner();
+    });
+});
+
+const showWinner= (winner) =>{
+    msg.innerText= ' Congratualtion, winner is ${winner}';
+    msgContainer.classList.remove("hide");
 }
-console.log("clicked");
+const checkWinner=() =>{
+    for( let pattern of winPattern){
+        
+        let pos1Val=boxes[pattern[0]].innerText;
+        let pos2Val =boxes[pattern[1]].innerText;
+        let pos3Val =boxes[pattern[2]].innerText;
+
+        if(pos1Val != "" && pos2Val != "" && pos3Val!=""){
+            if(pos1Val === pos2Val && pos2Val === pos3Val){
+                console.log("winner",pos1Val);
+                showWinner();
+            }
+        }
+    }
+}
